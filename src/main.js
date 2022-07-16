aa24ec6ce2032eda9eb4ece7397a8e3b5a2564ed
unknown <melbourne14gabriel@gmail.com>
2022-07-16 17:20:18 +0800

    js finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+const quoteSrc = document.querySelector('.quote-source')
+const quoteText = document.querySelector('.quote-text')
+const quoteAuthor = document.querySelector('.quote-author')
+const btn = document.querySelector('.btn')
+
+
+btn.addEventListener("click", getQuote)
 
 async function getQuote(){
     const response = await fetch('https://stoic-server.herokuapp.com/random')
@@ -7,7 +14,9 @@ async function getQuote(){
     const quote = data['0']
     
     console.log(quote)
-}
 
-getQuote()
+    quoteSrc.innerHTML = quote['quotesource']
+    quoteText.innerHTML = `\"${quote['body']}\"`
+    quoteAuthor.innerHTML = ` - ${quote['author']}`
+}
 
